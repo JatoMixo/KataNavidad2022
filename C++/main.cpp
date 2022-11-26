6672016f960110016718d6fdda588dba958461d8
JatoMixo <jatomixo@outlook.es>
2022-11-26 19:55:05 +0100

    main

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-void SetState(int initX, int initY, int endX, int endY, bool state, bool *array){
+void SetState(int initX, int initY, int endX, int endY, bool state, bool array[1000][1000]){
     int len = sizeof(array)/sizeof(array[0]);
-    std::cout << len;
+    int subLen = sizeof(array[0])/sizeof(array[0, 0]);
+    
+    for (int i = initX; i < endX; i++){
+        for (int j = initY; j < endY; j++){
+            *array[j, i] = state;
+        }
+    }
 }
--- C++/tests.cpp
+++ C++/tests.cpp
@@ -1,12 +1,12 @@
 #include <stdexcept>
-#include </home/jatomixo/Code/Crafters/KataNavidad/C++/main.cpp>
+#include <D:/Miguel/Programas/KataNavidad2022/C++/main.cpp>
 
 void IS_TRUE(bool condition, std::string exception){
     if (condition) return;
     throw new std::invalid_argument(exception);
 }
 void IS_FALSE(bool condition, std::string exception){
-    IS_TRUE(!condition, exception);;
+    IS_TRUE(!condition, exception);
 }
 int main() {
     bool grid[1000][1000];
