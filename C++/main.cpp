5dbca31f1adea4e65ab106b1412c1584a9fb7734
JatoMixo <jatomixo@outlook.es>
2022-11-29 17:05:32 +0100

    main

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 void ChangeState(int initX, int initY, int endX, int endY, bool array[1000][1000], bool state=NULL){
     int len = sizeof(array)/sizeof(array[0]);
     int subLen = sizeof(array[0])/sizeof(array[0, 0]);
